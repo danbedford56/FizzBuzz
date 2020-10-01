cc5fc13ee19e10071cbda23c39e9f1a3e762f00c
Kalindi Fonda <kalindi.fonda@gmail.com>
2020-10-01 11:17:55 +0100

    Add test for number

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,10 @@
 def fizzbuzz(number)
   if number === 15
   	return 'fizzbuzz'
-  elsif number == 5
+  elsif number  % 5 == 0
   	return 'buzz'
   elsif number % 3 == 0
     return 'fizz'
   end
+   return number
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,9 +5,14 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3 && 6 && 9 && 12 && 18)).to eq 'fizz'
   end
   it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(5 && 10 && 20)).to eq 'buzz'
   end
 	it 'returns "fizzbuzz" when passed 15' do
 		expect(fizzbuzz(15)).to eq 'fizzbuzz'
 	end
+  [1,2,4,7,8,11,13,14,16,17,19].each do |x|
+    it 'returns ' + x.to_s + ' when passed ' + x.to_s do
+      expect(fizzbuzz(x)).to eq x
+    end
+  end
 end
